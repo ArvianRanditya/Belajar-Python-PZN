ceae79e0cd1126f1ab2be49fc7506af3eb3cc722
ArvianRanditya <arvianrandityaa@gmail.com>
2025-07-30 20:38:39 +0700

    up

---
+++ 04_OperasiMath.py
@@ -0,0 +1,14 @@
+# TAMBAH
+print(1+2)
+
+# KURANG
+print(2-1)
+
+# KALI
+print(2*3)
+
+# BAGI
+print(4/2)
+
+# SISA BAGI
+print(5%2)
---
+++ 05_Variable.py
@@ -0,0 +1,15 @@
+# KETIKA KAMU INGIN MEMBUAT KATA "Hallo Dunia" DARIPADA MENGUBAH SATU PERSATU BENTUK PRINTNYA LEBIH EFISIEN MENGGUNAKAN VARIABEL
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+
+# SEPEPRTI INI CARA VARIABLE BERKERJA
+hello = "Hallo Dunia"
+print(hello)
+print(hello)
+print(hello)
+hello = "Hallo Python" # JIKA TERDAPAT VARIABEL YANG SAMA NILAI VARIABEL AKAN MENGIKUTI SESUAI URUTAN DEKLARASI
+print(hello)
+print(hello)
